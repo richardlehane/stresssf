d08c6bdec6385a27ae20d869e57f827ce47982af
Richard Lehane <richard.lehane@gmail.com>
2022-01-29 11:10:52 +0100

    silent logger

--- main.go
+++ main.go
@@ -8,18 +8,22 @@ import (
 	"net/url"
 	"os"
 	"path/filepath"
+	"strconv"
 	"sync"
+	"time"
 
 	retryablehttp "github.com/hashicorp/go-retryablehttp"
 )
 
-const corpusSz = 2048
-
 const groutines = 16
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Give a server e.g. localhost:8080")
+	if len(os.Args) < 3 {
+		log.Fatal("Give a server and corpus size e.g. stresssf localhost:8080 2048")
+	}
+	corpusSz, err := strconv.Atoi(os.Args[2])
+	if err != nil || corpusSz < 8 {
+		log.Fatal("Second argument (corpus size) must be an integer  of at least 8 e.g. stresssf localhost:8080 2048")
 	}
 	wg := &sync.WaitGroup{}
 	urls := make(chan string, groutines)
@@ -28,15 +32,20 @@ func main() {
 		go listen(urls, wg)
 	}
 	log.Println("Making corpus")
-	if err := makeCorpus(); err != nil {
+	if err := makeCorpus(corpusSz); err != nil {
 		log.Fatal(err)
 	}
-	for i := 0; i < corpusSz; i++ {
-		wg.Add(1)
-		urls <- makeUrl(os.Args[1], filepath.Join("corpus", fmt.Sprintf("%d.pdf", i)))
+	// Do multiple runs up to the size of the corpus
+	for i := 8; i <= corpusSz; i = i * 2 {
+		start := time.Now()
+		for j := 0; j < corpusSz; j++ {
+			wg.Add(1)
+			urls <- makeUrl(os.Args[1], filepath.Join("corpus", fmt.Sprintf("%d.pdf", j)))
+		}
+		wg.Wait()
+		elapsed := time.Since(start)
+		log.Printf("Run with corpus size %d took %s", i, elapsed)
 	}
-	log.Println("Waiting")
-	wg.Wait()
 	close(urls)
 }
 
@@ -44,7 +53,7 @@ func makeUrl(server, file string) string {
 	return "http://" + server + "/identify/" + url.QueryEscape(file) + "?format=json"
 }
 
-func makeCorpus() error {
+func makeCorpus(corpusSz int) error {
 	pdf := make([]byte, 25000)
 	copy(pdf, []byte{0x25, 0x50, 0x44, 0x46, 0x2D, 0x31, 0x2E, 0x34})
 	copy(pdf[len(pdf)-5:], []byte{0x25, 0x25, 0x45, 0x4F, 0x46})
@@ -68,14 +77,15 @@ func makeCorpus() error {
 }
 
 func listen(c chan string, wg *sync.WaitGroup) {
+	client := retryablehttp.NewClient()
+	client.Logger = nil
 	for url := range c {
-		resp, err := retryablehttp.Get(url)
+		resp, err := client.Get(url)
 		if err == nil {
-			i, _ := io.Copy(ioutil.Discard, resp.Body)
-			log.Printf("Got %s. Dumped %d bytes\n", url, i)
+			io.Copy(ioutil.Discard, resp.Body)
 			resp.Body.Close()
 		} else {
-			log.Fatal()
+			log.Fatal(err)
 		}
 		wg.Done()
 	}
